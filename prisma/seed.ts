e60fb9d72d4494303835633d612efabb98cf7f48
Chamika-Samarasinghe <120403416+Chamika-Samarasinghe@users.noreply.github.com>
2026-08-19 15:30:15 +0530

    Bug Fix 1.4
    fix: update seed.ts import to use @prisma/client

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,4 @@
-import "dotenv/config";
-import { PrismaClient } from "../lib/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const prisma = new PrismaClient({
